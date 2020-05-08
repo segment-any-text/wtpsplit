cd2ed38d8ad2917acb49b4b2e9809376f7c7d1f6
Benjamin Minixhofer <bminixhofer@gmail.com>
2020-05-08 20:42:29 +0200

    add browser matrix

--- bindings/javascript/cypress/integration/test.js
+++ bindings/javascript/cypress/integration/test.js
@@ -8,8 +8,7 @@ Cypress.on('window:before:load', (win) => {
 
 it('runs the javascript without errors and logs "cypress:success"', () => {
     cy.visit("/");
-    // TODO: add early exit
-    cy.wait(3000).then(() => {
+    cy.wait(5000).then(() => {
         expect(fail_spy).not.to.be.called;
         expect(success_spy).to.be.called;
     });
