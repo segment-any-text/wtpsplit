8b759282ddd599584713aa1b936e00c7c1cbb257
Benjamin Minixhofer <bminixhofer@gmail.com>
2020-06-25 20:00:52 +0100

    add extension module feature

--- bindings/python/tests/test_nnsplit.py
+++ bindings/python/tests/test_nnsplit.py
@@ -1,10 +1,12 @@
 import nnsplit
 
+
 def test_splitter_model_works():
     model = nnsplit.NNSplit.load("de")
     splits = model.split(["Das ist ein Test Das ist noch ein Test."])[0]
 
     assert [str(x) for x in splits] == ["Das ist ein Test ", "Das ist noch ein Test."]
+
 
 def test_splitter_model_works_with_args():
     model = nnsplit.NNSplit.load("de", threshold=1.0)
