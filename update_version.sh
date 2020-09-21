b0d4eafa199d5c0543ceec8bee615c877d480417
Benjamin Minixhofer <bminixhofer@gmail.com>
2020-09-21 10:36:10 +0200

    platform agnostic sed, simplify wheel upload

--- update_version.sh
+++ update_version.sh
@@ -1,8 +1,15 @@
+if [ `uname` = "Darwin" ]
+then
+    SED=gsed
+else
+    SED=sed
+fi
+
 function update_cargo_toml_version {
     VERSION=$1
     FILE=$2
 
-    sed -i "0,/^version/s/^version *= *\".*\"/version = \"$VERSION\"/" $FILE
+    $SED -i "0,/^version/s/^version *= *\".*\"/version = \"$VERSION\"/" $FILE
 }
 
 update_cargo_toml_version $1 nnsplit/Cargo.toml
