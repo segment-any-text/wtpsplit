d132e1f4bd6a629f7c9bdd3343c06ec8d9b1da53
Benjamin Minixhofer <bminixhofer@gmail.com>
2020-09-21 17:05:36 +0200

    add Node.js tests

---
+++ bindings/javascript/test/test.js
@@ -0,0 +1,19 @@
+const nnsplit = require('../pkg');
+const assert = require('assert');
+
+describe('NNSplit', function () {
+    it('should instantiate given valid .onnx path', async function () {
+        let splitter = await nnsplit.NNSplit.new("../../models/de/model.onnx");
+    });
+    it('should raise an error given an invald model', async function () {
+        assert.rejects(nnsplit.NNSplit.new("package.json"));
+    });
+    it('should split a simple text correctly', async function () {
+        let splitter = await nnsplit.NNSplit.new("../../models/de/model.onnx");
+        let split = await splitter.split([
+            "Das ist ein Test Das ist noch ein Test.",
+        ]);
+
+        assert.deepStrictEqual(split[0].parts.map((x) => x.text), ["Das ist ein Test ", "Das ist noch ein Test."]);
+    });
+});
