deb0fc15876143aeb7b4acb29354959f6ad98e54
Benjamin Minixhofer <bminixhofer@gmail.com>
2023-07-14 15:53:04 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="wtpsplit",
-    version="1.2.1",
+    version="1.2.2",
     packages=["wtpsplit"],
     description="Robust, adaptible sentence segmentation for 85 languages",
     author="Benjamin Minixhofer",
