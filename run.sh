d87d4d5979fd4da03d8dae075ecf5b6b7ee5c12f
markus583 <markus.frohmann@gmail.com>
2024-05-01 10:53:33 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # TODO: cleanup in case of no .arrow files but cache-* files available.
-python3 ~/wtpsplit/xla_spawn.py --num_cores ${TPU_NUM_DEVICES} wtpsplit/train/train_xlmr.py $1
+python3 ~/wtpsplit/xla_spawn.py --num_cores ${TPU_NUM_DEVICES} wtpsplit/train/train.py $1
