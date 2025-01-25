c9e0b49f7ee9a628a1e200d27a85db6c1aa4ce37
markus583 <markus.frohmann@gmail.com>
2025-01-25 17:41:20 +0100

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wtpsplit",
-    version="2.1.3",
+    version="2.1.4",
     packages=find_packages(),
     description="Universal Robust, Efficient and Adaptable Sentence Segmentation",
     author="Markus Frohmann, Igor Sterner, Benjamin Minixhofer",
